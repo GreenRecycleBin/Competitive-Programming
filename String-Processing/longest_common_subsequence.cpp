8243df79180b7ec00251ce4353908680b0e2b8d0
Daniel Le <GreenRecycleBin@gmail.com>
2012-11-04 10:38:54 +0800

    Fix minor error.

--- String-Processing/longest_common_subsequence.cpp
+++ String-Processing/longest_common_subsequence.cpp
@@ -12,7 +12,7 @@ int LongestCommonSubsequence(const string &a, const string &b)
   for (int i = 0; i <= b.size(); ++i) matrix[0][i] = 0;
 
   for (int i = 1; i <= a.size(); ++i) {
-    for (int j = 1; j <= a.size(); ++j) {
+    for (int j = 1; j <= b.size(); ++j) {
       if (a[i - 1] == b[j - 1]) matrix[i][j] = matrix[i - 1][j - 1] + 1;
       else matrix[i][j] = max(matrix[i][j - 1], matrix[i - 1][j]);
     }
